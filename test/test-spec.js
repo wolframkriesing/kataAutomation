af0050c60ad1b1ecc64f7ee579b44795c72d7fce
Georg Lang <georglang0788@gmx.net>
2014-02-13 15:00:39 +0100

    first test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,5 +1,61 @@
-describe('Test', function() {
-  it('test it', function(){
+function getMass(){
+  
+}
+
+
+describe('test', function() {
+  it('test', function(){
     expect(true).toBe(true);
   });
+
+  it('constant', function () {
+    expect(getMass('0')).toBe(1);
+  });
+
+//  it('declare variable', function () {
+//    expect(getMass('var x')).toBe(1);
+//  });
+//
+//  it('variabel and constant', function () { //binding
+//    expect(getMass('var x = 0')).toBe(2);
+//  });
+//
+//  it('call function', function () {
+//    expect(getMass('count()')).toBe(2);
+//  });
+//
+//  it('call function', function () {
+//    expect(getMass('count(4)')).toBe(3);
+//  });
+//
+//  it('call instance method without value', function () {
+//    expect(getMass('object.print()')).toBe(3);
+//  });
+//
+//  it('call instance method with value', function () {
+//    expect(getMass('object.print(3)')).toBe(4);
+//  });
+//
+//    it('call instance property', function () {
+//    expect(getMass('object.property')).toBe(3);
+//  });
+
+});
+
+
+var estraverse = require('../node_modules/estraverse/estraverse');
+var esprima = require('../node_modules/esprima/esprima');
+
+var traverse = function(tree, transform) {
+  estraverse.traverse(tree, {
+    enter: function(node) {
+      return transform(node, this.parents());
+    }
+  });
+};
+
+var tree = esprima.parse('x = 1');
+
+traverse(tree, function(node, parents) {
+  console.log('node', node);
 });
